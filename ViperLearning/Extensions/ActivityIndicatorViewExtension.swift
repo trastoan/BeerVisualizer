b6bf5470994f0961f8a1c33748e96669a623da04
trastoan <yurisaboiaf@gmail.com>
2017-12-08 17:35:15 -0300

    changed parameter name o activity indicator setup

--- ViperLearning/Extensions/ActivityIndicatorViewExtension.swift
+++ ViperLearning/Extensions/ActivityIndicatorViewExtension.swift
@@ -9,11 +9,11 @@
 import UIKit
 //Helps to setup activity indicator on view with autolayout
 extension UIActivityIndicatorView {
-    func setupOn(view: UIView, x: CGFloat = 0.0, y: CGFloat = 0.0) {
+    func setupOn(view: UIView, xFromCenter: CGFloat = 0.0, yFromCenter: CGFloat = 0.0) {
         view.addSubview(self)
         self.activityIndicatorViewStyle = .gray
         self.translatesAutoresizingMaskIntoConstraints = false
-        self.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: x).isActive = true
-        self.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: y).isActive = true
+        self.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: xFromCenter).isActive = true
+        self.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: yFromCenter).isActive = true
     }
 }
